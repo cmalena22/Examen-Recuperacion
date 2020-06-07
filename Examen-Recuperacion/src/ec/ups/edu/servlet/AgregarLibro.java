e53ec6abe0e3c5d51645d1c9241a02407e41365a
Cinthia iza <cizac1@est.ups.edu.ec>
2020-06-07 11:48:41 -0500

    Servlet

--- Examen-Recuperacion/src/ec/ups/edu/servlet/AgregarLibro.java
+++ Examen-Recuperacion/src/ec/ups/edu/servlet/AgregarLibro.java
@@ -75,12 +75,12 @@ public class AgregarLibro extends HttpServlet {
 		libro.setNumpaginas(numpag);		
 		//daolibro.create(libro);
 		
-		Libro envio =daolibro.retornar(nombre, isbn);
+		System.out.println(daolibro.retornar(nombre, isbn));
 		Autor envioau=daoautor.read(1);
 		System.out.println("Lo que voy enviar");
 		System.out.println(libro.toString());
 		System.out.println("Lo que recupero librooooooo");
-		System.out.println(envio.toString());
+		//System.out.println(envio.toString());
 		
 		System.out.println("Lo que envio autorrrrrrrrrrr");
 		System.out.println(envioau.toString());
